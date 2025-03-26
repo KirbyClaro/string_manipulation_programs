3baa841581e9df88dcf6abac38839e51dcdeee3b
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 17:32:40 +0800

    Creating a program that remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.

---
+++ remove_prefix.py
@@ -0,0 +1,5 @@
+#Prog02B6. removeprefix() remove the characters 
+# at the beginning of the string that matches the 
+# function parameter. Create a program that do 
+# the same functionality without using 
+# removeprefix() function.
