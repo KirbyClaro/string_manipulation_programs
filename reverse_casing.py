82fc421c3b9d2a0ce590be5d50dc56fbcfb2a659
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 19:11:35 +0800

    added comments for code readability

--- reverse_casing.py
+++ reverse_casing.py
@@ -1,17 +1,22 @@
-#Prog08. swapcase() reverse the casing of 
+#Prog08B6. swapcase() reverse the casing of 
 # each of the character of the string. 
 # a program that do the same functionality 
 # without using swapcase() function.
 
+# Get user input for the string
 user_input = input("Enter a string: ")
 
+# Reverse the casing of each character in the string
 result = ""
 for char in user_input:
     if "a" <= char <= "z":
+        #Turn Lowercase to  Uppercase
         result += chr(ord(char) - 32)  
     elif 'A' <= char <= 'Z':
+        # Turn Uppercase to Lowercase
         result += chr(ord(char) + 32)  
     else:
         result += char
 
+# Display the modified string
 print("Reverse Casing:", result)
