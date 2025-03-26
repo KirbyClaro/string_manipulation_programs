21429251f4e5dd59ea12f065e69562b38a88775e
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 18:32:06 +0800

    fixed comments

--- check_string_endswith.py
+++ check_string_endswith.py
@@ -1,4 +1,4 @@
-#Prog05. endswith() check if the string 
+#Prog05B6. endswith() check if the string 
 # end part matches the function parameter. 
 # Create a program that do the same 
 # functionality without using endswith() function.
--- convert_to_upper_case.py
+++ convert_to_upper_case.py
@@ -1,4 +1,4 @@
-#Prog04. isupper() check if all characters of the string 
+#Prog04B6. isupper() check if all characters of the string 
 # is on upper case. Create a program that 
 #do the same functionality without using isupper() function.
 
