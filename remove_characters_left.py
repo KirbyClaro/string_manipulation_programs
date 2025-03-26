f0f2b97c183c3f38bb5aa0f4f9e1c0fdd978f11a
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 17:22:11 +0800

    added def remove_leading_spaces with while and if statement to see if there is spaces in the left

--- remove_characters_left.py
+++ remove_characters_left.py
@@ -1,3 +1,10 @@
 #Prog01. lstrip() remove the space characters at the beginning 
 #of the string. Create a program that do the same 
 #functionality without using lstrip() function.
+
+def remove_leading_spaces(text):
+    position = 0
+    while position < len(text):
+        if text[position] != ' ':  # Found a non-space character
+            break
+        position += 1
