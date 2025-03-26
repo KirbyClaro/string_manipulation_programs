4f63aceb11bc8eb54c3939765ec2aeefb1cb69b3
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 19:05:52 +0800

    removed the space in output and added comments for code readability

--- first_letter_capital.py
+++ first_letter_capital.py
@@ -4,14 +4,18 @@
 # program that do the same functionality without 
 # using title() function.
 
+#user input
 user_input = input("Enter a string: ")
 
+# split the input string into words
 split_input = user_input.split()
 new_words = []
 
+# capitalize the first letter of each word and convert the rest to lower case
 for word in split_input:
     new_word = word[0].upper() + word[1:].lower() if word else "" 
     new_words.append(new_word)\
 
-result = " ".join(new_words)
+# join the words back together into a string and print the result
+result = "".join(new_words)
 print("Result: " + result)
