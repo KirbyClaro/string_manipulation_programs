2cad4facd5194f9db1a57b41bde3d0287b9f0b23
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 19:08:50 +0800

    added user input with result and starting for statement

--- reverse_casing.py
+++ reverse_casing.py
@@ -2,3 +2,8 @@
 # each of the character of the string. 
 # a program that do the same functionality 
 # without using swapcase() function.
+
+user_input = input("Enter a string: ")
+
+result = ""
+for char in user_input:
