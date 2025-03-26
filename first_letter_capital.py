6d8ac7d65d7f7edd3a44b7d4fb06b4d54eb9ae83
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 19:06:39 +0800

    fixed comment batch

--- first_letter_capital.py
+++ first_letter_capital.py
@@ -1,4 +1,4 @@
-#Prog10. title() makes all first letter of 
+#Prog10B6. title() makes all first letter of 
 # each word in the string, capital letter. 
 # And all other letter in small case. Create a 
 # program that do the same functionality without 
