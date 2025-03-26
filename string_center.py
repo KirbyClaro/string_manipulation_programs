34119a4661014844026376a3e045035de4918dc4
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 19:00:04 +0800

    added the function with the output also with repr to show the spaces in left and right

--- string_center.py
+++ string_center.py
@@ -7,3 +7,13 @@
 # Get user input
 user_input = input("Enter a string: ")
 width = int(input("Enter the total width: "))
+
+# Add spaces to the left and right if the string length is less than the specified width
+if len(user_input) < width:
+    total_spaces = width - len(user_input)
+    left_spaces = total_spaces //2
+    right_spaces = total_spaces - left_spaces
+    user_input = " " * left_spaces + user_input + " " * right_spaces\
+
+# Display the modified string and added repr to show the added spaces
+print("Result:", repr(user_input))
