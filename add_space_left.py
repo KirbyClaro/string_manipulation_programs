e29b93a4555b323151da1417c73476d0a24ddc43
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 18:52:17 +0800

    fixed the code and also added comments with repr to show the spaces

--- add_space_left.py
+++ add_space_left.py
@@ -3,3 +3,14 @@
 # of characters specifies in function parameter. 
 # Create a program that do the same functionality 
 # without using ljust() function.
+
+# Get user input
+user_input = input("Enter a string: ")
+width = int(input("Enter the total width: "))
+
+# Add spaces to the right if the string length is less than the specified width
+if len(user_input) < width:
+    user_input += " " * (width - len(user_input))  # Add spaces to the right
+
+# Display the modified string and added repr to show the added spaces
+print("Result:", repr(user_input))
