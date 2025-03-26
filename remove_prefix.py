9133e4d11991ed0e0a0b2b78a393d7e65cbd5497
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 18:08:31 +0800

    added a function that see if the user input is the same with prefix

--- remove_prefix.py
+++ remove_prefix.py
@@ -6,3 +6,8 @@
 
 user_input = input("Enter a string: ")
 prefix = input("Enter a prefix: ")
+
+def remove_prefix(user_input, prefix):
+    if user_input.startswith(prefix):
+        return user_input[len(prefix):]
+    return user_input
