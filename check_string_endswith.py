087b8d01481edd81499fc9dd9aa7a9a36a12cfda
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 18:41:09 +0800

    fixed the error with the function that compare user_input with suffix and added comments for code readability

--- check_string_endswith.py
+++ check_string_endswith.py
@@ -3,12 +3,15 @@
 # Create a program that do the same 
 # functionality without using endswith() function.
 
+# Prompt the user to enter a string and a suffix
 user_input = input("Enter a string: ")
 suffix = input("Enter a suffix: ")
 
-def check_endswith(user_input, string):
-    return user_input[-len(suffix)] == suffix if len(suffix) <= len(user_input) else False
+# Function to check if the string ends with the given suffix
+def check_endswith(user_input, suffix):
+    return user_input[-len(suffix):] == suffix if len(suffix) <= len(user_input) else False
 
+# Check if the string ends with the given suffix and display the result
 if check_endswith(user_input, suffix):
     print("The string ends with the suffix.")
 else:
