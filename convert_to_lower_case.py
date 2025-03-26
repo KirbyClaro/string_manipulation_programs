78ec5a119bba47e06271850ce0c4dd4a55976233
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 18:19:15 +0800

    Added the def with the result and output

--- convert_to_lower_case.py
+++ convert_to_lower_case.py
@@ -6,3 +6,13 @@
 user_input = input("Enter a string: ")
 
 def lower_user_input(user_input):
+    result = ""
+    for char in user_input:
+        if "A" <= char <= "Z":
+            result += chr(ord(char) + 32)
+        else:
+            result += char
+        return result
+
+result = lower_user_input(user_input)
+print("The lower case version of the string is:", lower_user_input(user_input))
