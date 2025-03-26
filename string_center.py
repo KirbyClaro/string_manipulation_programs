61cbe0b1fecd68c998c2904173f66d87acbd86ab
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 18:57:42 +0800

    reused INPUT code from add_space_right

--- string_center.py
+++ string_center.py
@@ -3,3 +3,7 @@
 # string to print the string at the center. 
 # Create a program that do the same functionality 
 # without using center() function.
+
+# Get user input
+user_input = input("Enter a string: ")
+width = int(input("Enter the total width: "))
