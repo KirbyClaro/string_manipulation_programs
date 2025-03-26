53714cff0766c029a23c474485d3193ac3380210
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 18:39:15 +0800

    added def that compare the string with suffix then return true if the suffix == user_input and false if not

--- check_string_endswith.py
+++ check_string_endswith.py
@@ -2,3 +2,14 @@
 # end part matches the function parameter. 
 # Create a program that do the same 
 # functionality without using endswith() function.
+
+user_input = input("Enter a string: ")
+suffix = input("Enter a suffix: ")
+
+def check_endswith(user_input, string):
+    return user_input[-len(suffix)] == suffix if len(suffix) <= len(user_input) else False
+
+if check_endswith(user_input, suffix):
+    print("The string ends with the suffix.")
+else:
+    print("The string does not end with the suffix.")
