b83ef5a8dcbdc63958e29ab82a444dc4fbf6395e
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-26 18:45:45 +0800

    Creating a program that add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.

---
+++ add_space_left.py
@@ -0,0 +1,5 @@
+#Prog06B6. ljust() add space characters at the 
+# end of the string to complete the number 
+# of characters specifies in function parameter. 
+# Create a program that do the same functionality 
+# without using ljust() function.
