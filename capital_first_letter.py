fd5f2b329e35a0bb112465cafcd9b980ce4321ab
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-27 23:47:31 +0800

    Fix the code error by moving the def first then input in the bottom and also by adding () to functions and also code comments for code readability

--- capital_first_letter.py
+++ capital_first_letter.py
@@ -4,12 +4,18 @@
 # do the same functionality without using 
 # capitalize() function.
 
-user_input = input("Enter a string: ")
-result = capitalization(user_input)
 
+# Function to capitalize the first letter of the string and convert the rest to lowercase
 def capitalization(user_input):
     if len(user_input) == 0:
         return user_input
-    return user_input[0].upper + user_input[1:].lower
+    return user_input[0].upper() + user_input[1:].lower()
 
+# Get user input
+user_input = input("Enter a string: ")
+
+# Call the function and store the result in the variable "result"
+result = capitalization(user_input)
+
+# Print the capitalized string
 print("Capitalized string: ", result)
