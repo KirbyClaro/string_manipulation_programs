d991cedded49ce8ee44a8bb390a56844b6703408
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-27 23:42:44 +0800

    added inputs and result variable to initialize the variable

--- capital_first_letter.py
+++ capital_first_letter.py
@@ -3,3 +3,9 @@
 # letter in small case. Create a program that 
 # do the same functionality without using 
 # capitalize() function.
+
+user_input = input("Enter a string: ")
+result = capitalization(user_input)
+
+def capitalization(user_input):
+    if len(user_input) == 0
