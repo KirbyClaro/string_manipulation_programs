f5283c8e3d342688aada76d4325b2cd85021d918
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 14:44:42 +0800

    fixed comments

--- python_string_functions/right_strip_other_method.py
+++ python_string_functions/right_strip_other_method.py
@@ -1,4 +1,4 @@
-#Prog01. rstrip() remove the space characters 
+#Prog01B7. rstrip() remove the space characters 
 # at the end of the string. Create a 
 # program that do the same functionality 
 # without using rstrip() function.
