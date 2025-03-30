3842b860e08b47d1ec7fcc37ad620f8d08c288e5
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 15:39:13 +0800

    added comments for code readability

--- python_string_functions/add_zero_fill_other_method.py
+++ python_string_functions/add_zero_fill_other_method.py
@@ -4,10 +4,13 @@
 #Create a program that do the same functionality 
 #without using zfill() function.
 
+# Get user input for the string and total width
 user_input = input("Enter a string: ")
 total_width = int(input("Enter the total width: "))
 
+# Add zero characters at the beginning if the string length is less than the specified width
 while len(user_input) < total_width:
     user_input = "0" + user_input
-
+    
+# Display the modified string and added repr to show the added zeros
 print("Result:", repr(user_input))
