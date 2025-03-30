36de2a9f0e9d3491ed28ef59732a03e4aeef1294
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 15:18:26 +0800

    added user input and is_lower with a value of true

--- python_string_functions/convert_to_lowercase.py
+++ python_string_functions/convert_to_lowercase.py
@@ -2,3 +2,6 @@
 # characters of the string is on lower case. 
 # Create a program that do the same functionality 
 # without using islower() function.
+
+user_input = input("Enter a string: ")
+is_lower = True
