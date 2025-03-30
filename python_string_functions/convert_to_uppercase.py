6738db0c05ec7721495889b77946f855e8f5aa9a
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 15:16:04 +0800

    added comment for code readability

--- python_string_functions/convert_to_uppercase.py
+++ python_string_functions/convert_to_uppercase.py
@@ -3,13 +3,16 @@
 #Create a program that do the same 
 #functionality without using upper() function.
 
+# Get user input for the string
 user_input = input("Enter a string: ")
 uppercase_user_input = ""
 
+# Convert lowercase letters to uppercase by subtracting 32 from their ASCII values
 for char in user_input:
     if 'a' <= char <= 'z':  
         uppercase_user_input += chr(ord(char) - 32)
     else:
         uppercase_user_input += char
         
+# Display the modified string
 print("Uppercase string:", uppercase_user_input)
