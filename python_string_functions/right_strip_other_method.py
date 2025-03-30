79c6180ec87efe9de6561f99946f41b28bcd717a
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 14:42:42 +0800

    added user input and while statement to remove trailing spaces in the right side

--- python_string_functions/right_strip_other_method.py
+++ python_string_functions/right_strip_other_method.py
@@ -2,3 +2,9 @@
 # at the end of the string. Create a 
 # program that do the same functionality 
 # without using rstrip() function.
+
+user_input = input("Enter a string: ")
+while len(user_input) > 0 and user_input[-1] == " ":
+    user_input = user_input[:-1]
+    
+print("String after removing trailing spaces:", user_input)
