26dda2fb44d0ab0cdaf42d395c9402b3895e7733
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 15:25:16 +0800

    added if statement that uses a boolean to determine if prefix and string matches

--- python_string_functions/check_if_prefix_matches.py
+++ python_string_functions/check_if_prefix_matches.py
@@ -6,3 +6,13 @@
 user_input = input("Enter a string: ")
 prefix = input("Enter a prefix: ")
 matches = True
+
+if len(prefix) > len(user_input):
+    matches = False
+else:
+    for i in range(len(prefix)):
+        if user_input[i] != prefix[i]:
+            matches = False
+            break
+
+print("Does the string match with the function parameter(prefix)?", matches)
