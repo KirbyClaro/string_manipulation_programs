be6cbb75117055d28c3ad9ac3e1efec57978a35c
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 15:19:30 +0800

    reused some parts of convert to uppercase and added output with comments for code readability

--- python_string_functions/convert_to_lowercase.py
+++ python_string_functions/convert_to_lowercase.py
@@ -5,3 +5,12 @@
 
 user_input = input("Enter a string: ")
 is_lower = True
+
+# iterate over each character in the input string and check if any character is uppercase
+for char in user_input:
+    if 'A' <= char <= 'Z':
+        is_lower = False
+        break
+
+#print is_lower
+print("Is the string all lowercase?", is_lower)
