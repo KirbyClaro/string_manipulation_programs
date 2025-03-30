69d0e07cedba0dd4b513f1f7e9661989f12e3ec2
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 15:26:10 +0800

    fixed some comments

--- python_string_functions/check_if_prefix_matches.py
+++ python_string_functions/check_if_prefix_matches.py
@@ -1,4 +1,4 @@
-#Prog05. startswith() check if the string 
+#Prog05B7. startswith() check if the string 
 #beginning part matches the function parameter. 
 #Create a program that do the same functionality 
 #without using startswith() function.
