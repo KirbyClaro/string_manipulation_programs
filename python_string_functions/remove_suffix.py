bf890a72d40d84157cbb4fe7d617a59927856b18
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 14:46:43 +0800

    Creating a program that remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.

---
+++ python_string_functions/remove_suffix.py
@@ -0,0 +1,5 @@
+#Prog02B7. removesuffix() remove the 
+#characters at the end of the string 
+#that matches the function parameter. 
+#Create a program that do the same 
+#functionality without using removesuffix() function.
