96ab53774a3550d2be90ef7d8098e87881b7696a
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 14:43:59 +0800

    added repr to show the output and comments for code readability

--- python_string_functions/right_strip_other_method.py
+++ python_string_functions/right_strip_other_method.py
@@ -3,8 +3,12 @@
 # program that do the same functionality 
 # without using rstrip() function.
 
+#user input
 user_input = input("Enter a string: ")
+
+# remove the trailing spaces using a loop
 while len(user_input) > 0 and user_input[-1] == " ":
     user_input = user_input[:-1]
     
-print("String after removing trailing spaces:", user_input)
+# print the resulting string
+print("String after removing trailing spaces:", repr(user_input))
