dfec19b6022343d1b01e26c59a032062d46fcc9d
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 15:09:37 +0800

    added user input with if statement to check if user input is the same with suffix them remove

--- python_string_functions/remove_suffix.py
+++ python_string_functions/remove_suffix.py
@@ -4,8 +4,13 @@
 #Create a program that do the same 
 #functionality without using removesuffix() function.
 
+# Get user input for the string and suffix
 user_input = input("Enter a string: ")
 suffix = input("Enter a suffix to remove")
 
+# Check if the suffix exists in the string
 if user_input.endswith(suffix):
     user_input = user_input[:len(user_input)-len(suffix)]
+    
+#print user_input
+print("String after removing suffix:", user_input)
