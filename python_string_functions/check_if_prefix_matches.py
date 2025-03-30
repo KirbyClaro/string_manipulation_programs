e044955c0c476b34a6b6e91446d3151abc49ced1
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 15:25:48 +0800

    added comments for code readability

--- python_string_functions/check_if_prefix_matches.py
+++ python_string_functions/check_if_prefix_matches.py
@@ -3,16 +3,20 @@
 #Create a program that do the same functionality 
 #without using startswith() function.
 
+#user input
 user_input = input("Enter a string: ")
 prefix = input("Enter a prefix: ")
 matches = True
 
+# Check if the string starts with the given prefix
 if len(prefix) > len(user_input):
     matches = False
 else:
+    # compare characters one by one from both strings
     for i in range(len(prefix)):
         if user_input[i] != prefix[i]:
             matches = False
             break
 
+# Display the result
 print("Does the string match with the function parameter(prefix)?", matches)
