5cfc3378c1ba2b66ac478a31751df1cbe0fb5d1c
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 14:50:07 +0800

    added user input and suffix also added if statement to check if the ending

--- python_string_functions/remove_suffix.py
+++ python_string_functions/remove_suffix.py
@@ -3,3 +3,9 @@
 #that matches the function parameter. 
 #Create a program that do the same 
 #functionality without using removesuffix() function.
+
+user_input = input("Enter a string: ")
+suffix = input("Enter a suffix to remove")
+
+if user_input.endswith(suffix):
+    user_input = user_input[:len(user_input)-len(suffix)]
