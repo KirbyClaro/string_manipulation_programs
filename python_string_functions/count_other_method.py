e21ee990ff752aa64e5fdd295a5000584d8eed75
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 15:43:09 +0800

    reused some code from the zero fill program and and used count to count how many times that specific character appear

--- python_string_functions/count_other_method.py
+++ python_string_functions/count_other_method.py
@@ -2,3 +2,13 @@
 #the function parameter appear in the string. 
 #Create a program that do the same functionality 
 #without using count() function.
+
+user_input = input("Enter a string: ")
+character_count = input("Enter a character to count: ")
+count = 0
+
+for c in user_input:
+    if c == character_count:
+        count += 1
+
+print("The character", character_count, "appears", count, "times in the string.")
