7fe959e492aba51fd66b25f8d0e96dc81e9738e6
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 15:45:26 +0800

    added user input and character finder with index of -1

--- python_string_functions/index_other_method.py
+++ python_string_functions/index_other_method.py
@@ -2,3 +2,7 @@
 #location of the function parameter in the string. 
 #Create a program that do the same functionality 
 #without using index() function.
+
+user_input = input("Enter a string: ")
+character_finder = input("Enter a character to find: ")
+index = -1 
