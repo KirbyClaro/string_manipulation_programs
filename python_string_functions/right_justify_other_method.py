4ebd1ac739d2a22c84a70837ea256132c3e58c76
KirbyClaro <kirbychristianclaro@gmail.com>
2025-03-30 15:34:42 +0800

    fixed errors with int and in while statement and added comments for code readability

--- python_string_functions/right_justify_other_method.py
+++ python_string_functions/right_justify_other_method.py
@@ -5,10 +5,13 @@
 # Create a program that do the same 
 # functionality without using rjust() function.
 
+# Get user input for the string and total width
 user_input = input("Enter a string: ")
-total_width = input("Enter total width: ")
+total_width = int(input("Enter total width: "))
 
+# Add space characters at the beginning to make the string length equal to total width
 while len(user_input) < total_width:
-    user_input = ' ' + user_input
+    user_input = " " + user_input
 
+# Display the modified string and added repr to show the added spaces
 print("This is the right justified string: ", repr(user_input))
